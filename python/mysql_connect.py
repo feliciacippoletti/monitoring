187ebdfde6c48f1be6da105d766e5d151cbbe726
root <root@fedora.localhost>
2017-06-05 18:55:14 -0700

    Starting monitoring of metadata locks

--- python/mysql_connect.py
+++ python/mysql_connect.py
@@ -20,9 +20,26 @@ def connectMySQL(db_name):
      return False
 
 def findLocks():
-    cur.execute("SELECT * FROM INNODB_LOCK_WAITS")
+    #check if metadata_locks has any rows
+    cur.execute("SELECT ENABLED, TIMED FROM setup_instruments WHERE NAME = 'wait/lock/metadata/sql/mdl'")
+
     results = cur.fetchone()
     print results
+    if 'NO' in results:
+        print 'disabled'
 
-conn, cur = connectMySQL("information_schema")
+        cur.execute("UPDATE setup_instruments SET ENABLED = 'YES', TIMED = 'YES' WHERE NAME = 'wait/lock/metadata/sql/mdl';")
+        results = cur.fetchone()
+        print results
+
+    else:
+        cur.execute("SELECT * FROM metadata_locks where LOCK_STATUS = 'PENDING'")
+        results = cur.fetchone()
+        #if 'None' in results:
+         #   return
+        #else:
+            #send email to team notifying of hang, possible query/job is hanging
+         #   return
+
+conn, cur = connectMySQL("performance_schema")
 findLocks()
